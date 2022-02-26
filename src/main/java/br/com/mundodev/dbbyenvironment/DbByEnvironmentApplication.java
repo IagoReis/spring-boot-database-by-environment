0135503a28020a8dbf9b778e669ce6ce71da454b
Iago Reis <iagoidrs@gmail.com>
2022-02-26 20:53:17 -0300

    Ah shit, here we go again!

---
+++ src/main/java/br/com/mundodev/dbbyenvironment/DbByEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.mundodev.dbbyenvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbByEnvironmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbByEnvironmentApplication.class, args);
+	}
+
+}
